5f28a428e4e125dc1e613e1b64e9e8c0f77a18bb
RedRocksNet <chenqun@gmail.com>
2026-05-15 19:30:09 -0600

    Publish update

--- reading/assets/app.js
+++ reading/assets/app.js
@@ -684,7 +684,7 @@ function getRevealDuration(chunk, charCount, durationMultiplier = 1) {
   const speedMultiplier = getSpeedMultiplier();
   return Math.max(
     minRevealMs,
-    Math.min(maxRevealMs, Math.round(base * speedMultiplier * durationMultiplier * 50))
+    Math.min(maxRevealMs, Math.round(base * speedMultiplier * durationMultiplier * 100))
   );
 }
 
